d6bfe47a181bd2696e46580964af51e29a5f5e1b
Gustavus Shaw II <gshaw1997@gmail.com>
2018-02-14 12:29:31 -0500

    Update env var for AWS

--- src/config/aws-sdk/config.ts
+++ src/config/aws-sdk/config.ts
@@ -3,8 +3,8 @@ env.config();
 
 export const AWS_SDK_CONFIG = {
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
-    region: process.env.REGION,
+    region: process.env.AWS_REGION,
 };
