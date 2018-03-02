a75c778010838ab67155ff50a6ee83cbee7c7df1
Nick Winner <design@winnerdigital.net>
2018-03-02 13:09:39 -0500

    Reconfigure cors and jwt config to allow access to ota codes routes, add route for logging out

--- src/middleware/jwt.config.ts
+++ src/middleware/jwt.config.ts
@@ -14,6 +14,15 @@ export const enforceTokenAccess = jwt({
   },
 }).unless({
   // Routes that don't require authorization
-  path: ['/api', '/api/users/ota-codes', '/api/users/tokens', /\/api\/users\/[A-Z,a-z,0-9,_]+\/tokens/i, /\/api\/learning-object/i],
+  path: [
+    '/api',
+    '/api/users/ota-codes',
+    '/api/users/tokens', 
+    /\/api\/users\/[A-Z,a-z,0-9,_]+\/tokens/i,
+    /\/api\/learning-object/i,
+    { url: '/api/users', methods: ['POST']  }, // register
+    '/api/users/ota-codes', // all ota-code routes do their own verifcation outsides of JWT
+    { url: '/api/users/tokens', methods: ['POST'] }, // login
+   ],
 });
 // TODO: Whitelist user routes
