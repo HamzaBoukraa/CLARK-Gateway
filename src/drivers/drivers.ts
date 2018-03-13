a5997245f6293a8155f03bbfeae9fe51f66625b8
Gustavus Shaw II <gshaw1997@gmail.com>
2018-03-13 13:04:44 -0400

    Remove DataStore

--- src/drivers/drivers.ts
+++ src/drivers/drivers.ts
@@ -1,4 +1,3 @@
-import { DBInteractionConnector } from './DBInteractionConnector';
 import { ExpressResponder } from './express/ExpressResponder';
 import { ExpressDriver } from './express/ExpressDriver';
 import ExpressRouteDriver from './express/ExpressRouteDriver';
@@ -7,5 +6,4 @@ export {
   ExpressDriver,
   ExpressResponder,
   ExpressRouteDriver,
-  DBInteractionConnector,
 };
