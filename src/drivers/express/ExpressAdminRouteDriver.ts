a9952a3aad941100ce3349d38100dd2e9605921e
Gustavus Shaw II <gshaw1997@gmail.com>
2018-05-03 13:09:44 -0400

    Add admin routes for learning objects

---
+++ src/drivers/express/ExpressAdminRouteDriver.ts
@@ -0,0 +1,116 @@
+import * as express from 'express';
+import { Router } from 'express';
+import * as proxy from 'express-http-proxy';
+import { ExpressResponder } from '../drivers';
+import * as querystring from 'querystring';
+import * as dotenv from 'dotenv';
+import { ADMIN_LEARNING_OBJECT_ROUTES } from '../../environment/routes';
+
+dotenv.config();
+const USERS_API = process.env.USERS_API || 'localhost:4000';
+const LEARNING_OBJECT_SERVICE_URI =
+  process.env.LEARNING_OBJECT_SERVICE_URI || 'localhost:5000';
+
+/**
+ * Serves as a factory for producing a router for the express app.rt
+ *
+ * @author Gustavus Shaw II
+ */
+export default class ExpressAdminRouteDriver {
+  /**
+   * Produces a configured express router
+   *
+   * @param dataStore the data store that the routes should utilize
+   */
+  public static buildRouter() {
+    let e = new ExpressAdminRouteDriver();
+    let router: Router = express.Router();
+    e.setRoutes(router);
+    return router;
+  }
+
+  private constructor() {}
+
+  getResponder(res) {
+    // TODO: Should this be some sort of factory pattern?
+    return new ExpressResponder(res);
+  }
+
+  /**
+   * Defines the active routes for the API. Routes take an async callback function that contains a request and response object.
+   * The callback awaits a particular interactor function that executes the connected business use case.
+   *
+   * @param router the router being used by the webserver
+   */
+  setRoutes(router: Router) {
+    router.get('/', function(req, res) {
+      res.json({
+        message: 'Welcome to the Admin C.L.A.R.K. Gateway API',
+      });
+    });
+
+    router.get(
+      '/learning-objects',
+      proxy(LEARNING_OBJECT_SERVICE_URI, {
+        proxyReqPathResolver: req => {
+          const route = `${
+            ADMIN_LEARNING_OBJECT_ROUTES.FETCH_LEARNING_OBJECTS
+          }?${querystring.stringify(req.query)}`;
+          return route;
+        },
+      }),
+    );
+    router.patch(
+      '/users/:username/learning-objects/:learningObjectName/publish',
+      proxy(LEARNING_OBJECT_SERVICE_URI, {
+        proxyReqPathResolver: req => {
+          const username = req.params.username;
+          const learningObjectName = req.params.learningObjectName;
+          return ADMIN_LEARNING_OBJECT_ROUTES.PUBLISH_LEARNING_OBJECT(
+            username,
+            learningObjectName,
+          );
+        },
+      }),
+    );
+    router.patch(
+      '/users/:username/learning-objects/:learningObjectName/unpublish',
+      proxy(LEARNING_OBJECT_SERVICE_URI, {
+        proxyReqPathResolver: req => {
+          const username = req.params.username;
+          const learningObjectName = req.params.learningObjectName;
+          return ADMIN_LEARNING_OBJECT_ROUTES.UNPUBLISH_LEARNING_OBJECT(
+            username,
+            learningObjectName,
+          );
+        },
+      }),
+    );
+    router.delete(
+      '/users/:username/learning-objects/:learningObjectName/publish',
+      proxy(LEARNING_OBJECT_SERVICE_URI, {
+        proxyReqPathResolver: req => {
+          const username = req.params.username;
+          const learningObjectName = req.params.learningObjectName;
+          return ADMIN_LEARNING_OBJECT_ROUTES.DELETE_LEARNING_OBJECT(
+            username,
+            learningObjectName,
+          );
+        },
+      }),
+    );
+    router.delete(
+      '/users/:username/learning-objects/multiple/:learningObjectIDs',
+      proxy(LEARNING_OBJECT_SERVICE_URI, {
+        proxyReqPathResolver: req => {
+          const username = req.params.username;
+          const learningObjectIDs = req.params.learningObjectIDs;
+          return ADMIN_LEARNING_OBJECT_ROUTES.DELETE_MULTIPLE_LEARNING_OBJECTS(
+            username,
+            learningObjectIDs,
+          );
+        },
+      }),
+    );
+  }
+}
--- src/environment/routes.ts
+++ src/environment/routes.ts
@@ -2,15 +2,15 @@ import * as querystring from 'querystring';
 export const LEARNING_OBJECT_ROUTES = {
   CREATE_UPDATE_LEARNING_OBJECT: '/learning-objects',
   LOAD_LEARNING_OBJECT(username: string, learningObjectName: string) {
-    return `/learning-objects/${username}/${encodeURIComponent(
-      learningObjectName,
-    )}`;
+    return `/learning-objects/${encodeURIComponent(
+      username,
+    )}/${encodeURIComponent(learningObjectName)}`;
   },
   LOAD_LEARNING_OBJECT_SUMARY: '/learning-objects/summary',
   FIND_LEARNING_OBJECT(username: string, learningObjectName: string) {
-    return `/learning-objects/${username}/${encodeURIComponent(
-      learningObjectName,
-    )}/id`;
+    return `/learning-objects/${encodeURIComponent(
+      username,
+    )}/${encodeURIComponent(learningObjectName)}/id`;
   },
   DELETE_LEARNING_OBJECT(learningObjectName: string) {
     return `/learning-objects/${encodeURIComponent(learningObjectName)}`;
@@ -24,7 +24,7 @@ export const LEARNING_OBJECT_ROUTES = {
   },
   FETCH_LEARNING_OBJECTS: '/learning-objects',
   FETCH_USERS_LEARNING_OBJECTS(username: string) {
-    return `/users/${username}/learning-objects`;
+    return `/users/${encodeURIComponent(username)}/learning-objects`;
   },
 
   UPLOAD_MATERIALS: `/files`,
@@ -36,6 +36,35 @@ export const LEARNING_OBJECT_ROUTES = {
 
 export const BUSINESS_CARD_ROUTES = {
   CARD(username: string, query: any) {
-    return `/users/${username}/cards?${querystring.stringify(query)}`;
+    return `/users/${encodeURIComponent(
+      username,
+    )}/cards?${querystring.stringify(query)}`;
+  },
+};
+
+export const ADMIN_LEARNING_OBJECT_ROUTES = {
+  FETCH_LEARNING_OBJECTS: `/admin/learning-objects`,
+  FETCH_LEARNING_OBJECTS_WITH_FILTER(query) {
+    return `/admin/learning-objects?${query}`;
+  },
+  PUBLISH_LEARNING_OBJECT(username, learningObjecID) {
+    return `/admin/users/${encodeURIComponent(
+      username,
+    )}/learning-objects/${learningObjecID}/publish`;
+  },
+  UNPUBLISH_LEARNING_OBJECT(username, learningObjecID) {
+    return `/admin/users/${encodeURIComponent(
+      username,
+    )}/learning-objects/${learningObjecID}/unpublish`;
+  },
+  DELETE_LEARNING_OBJECT(username, learningObjecID) {
+    return `/admin/users/${encodeURIComponent(
+      username,
+    )}/learning-objects/${learningObjecID}`;
+  },
+  DELETE_MULTIPLE_LEARNING_OBJECTS(username, learningObjectIDs) {
+    return `/admin/users/${encodeURIComponent(
+      username,
+    )}/learning-objects/multiple/${learningObjectIDs}`;
   },
 };
