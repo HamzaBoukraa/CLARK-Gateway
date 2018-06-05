0a23f2a6d48395de3bb4f535e36cd348aa66b5c3
Sean Donnelly <sdonnelly3000@gmail.com>
2018-06-05 14:23:20 -0400

    Remove error log for invalid access token

--- src/drivers/express/ExpressDriver.ts
+++ src/drivers/express/ExpressDriver.ts
@@ -1,17 +1,16 @@
 import * as express from 'express';
 import * as path from 'path';
-import * as helmetConfig from '../../middleware/helmet';
+import * as helmetConfig from '../middleware/helmet';
 import * as bodyParser from 'body-parser';
 import * as logger from 'morgan';
 import * as http from 'http';
-import { enforceTokenAccess } from '../../middleware/jwt.config';
-import { sentry } from '../../logging/sentry';
+import { enforceTokenAccess } from '../middleware/jwt.config';
+import { sentry } from '../middleware/logging/sentry';
 import { DataStore } from '../../interfaces/DataStore';
 import { ExpressRouteDriver } from '../drivers';
 import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser';
 import * as socketio from 'socket.io';
-import { ConstructorOptions } from 'raven';
 import { SocketInteractor } from '../../interactors/SocketInteractor';
 
 /**
@@ -43,7 +42,6 @@ export class ExpressDriver {
     this.app.use(enforceTokenAccess);
     this.app.use(function(error, req, res, next) {
       if (error.name === 'UnauthorizedError') {
-        sentry.logError('Invalid Access Token', 401);
         res.status(401).send('Invalid Access Token');
       }
     });
