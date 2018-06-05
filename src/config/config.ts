a6c0284e350d0f2e4f5244f902c8112a9839a03f
Sean Donnelly <sdonnelly3000@gmail.com>
2018-06-05 14:23:28 -0400

    Clean up folder structure

--- src/config/config.ts
+++
@@ -1,6 +0,0 @@
-export const key = 'somerandomkeythatwillbegeneratedinthefutureforsecuritystuff';
-// TODO: Check issuer requirements - should it be CLARK?
-// export const issuer = 'cube';
-export const issuer = 'bloomin_onion';
-export const SENTRY_URI = 'https://1fd3f26d01db4b1b88b3f466ee2bcf0c:11c741ba08424d60b50e9adb2e253621@sentry.io/270207';
-export const LO_SUGGESTION_URI = 'http://54.92.208.221:27015';
