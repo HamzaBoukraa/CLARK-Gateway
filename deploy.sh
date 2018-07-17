a25a23612e7e97d5124c8bbe497f92eee2298108
Gustavus Shaw II <gshaw1997@gmail.com>
2018-07-17 11:57:34 -0400

    Update deploy script. Switch docker image for alpine

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-SERVICE_NAME=<YOUR SERVICE NAME>
-CLUSTER_NAME=<YOUR CLUSTER NAME>
-BUILD_NUMBER=${CIRCLE_BUILD_NUM}
-IMAGE_TAG=${CIRCLE_SHA1}
-TASK_FAMILY=<YOUR TASK FAMILY>
+expr='.serviceArns[]|select(contains("${ECS_SERVICE_NAME}"))|split("/")|.[1]'
+SNAME=$(aws ecs list-services --output json --cluster $ECS_CLUSTER_NAME | jq -r $expr)
 
 # Create a new task definition for this build
-sed -e "s;%IMAGE_TAG%;${IMAGE_TAG};g" ecs-task-template.json > my_website-${BUILD_NUMBER}.json
-aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file://my_website-${BUILD_NUMBER}.json
+OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $ECS_TASK_NAME --output json)
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
+FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
 
 # Update the service with the new task definition and desired count
-TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
-DESIRED_COUNT=`aws ecs describe-services --cluster ${CLUSTER_NAME} --services ${SERVICE_NAME} | egrep "desiredCount" | head -1 | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
-if [ ${DESIRED_COUNT} = "0" ]; then
-    DESIRED_COUNT="1"
-fi
-
-aws ecs update-service --cluster ${CLUSTER_NAME} --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
+aws ecs register-task-definition --family $ECS_TASK_NAME --cli-input-json "$(echo $FINAL_TASK)"
+aws ecs update-service --service $SNAME --task-definition $ECS_TASK_NAME --cluster $ECS_CLUSTER_NAME
